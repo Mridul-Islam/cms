84e7c1e7e63dcb04fbe0883e908f8d4a6fa6ea69
Mridul-Islam <1610947@iub.edu.bd>
2021-08-04 16:30:08 +0600

    done custom search and show all category into sidebar

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -1,20 +1,9 @@
 <div class="col-md-4">
 
-
-    <?php
-
-        if (isset($_POST['submit'])) {
-            $search = $_POST['search'];
-            echo $search;
-        }
-
-    ?>
-
-
     <!-- Blog Search Well -->
     <div class="well">
         <h4>Blog Search</h4>
-        <form action="" method="post">
+        <form action="search.php" method="post">
             <div class="input-group">
                 <input type="text" name="search" class="form-control">
                 <span class="input-group-btn">
@@ -29,33 +18,36 @@
 
 
 
+
+
     <!-- Blog Categories Well -->
     <div class="well">
+
+        <?php
+
+        $query = "SELECT * FROM categories";
+        $query_result = mysqli_query($connection, $query);
+
+        if(!$query_result){
+            die("Qeury Failed" . mysqli_error($connection));
+        }
+
+
+        ?>
+
         <h4>Blog Categories</h4>
         <div class="row">
-            <div class="col-lg-6">
+            <div class="col-lg-12">
                 <ul class="list-unstyled">
-                    <li><a href="#">Category Name</a>
-                    </li>
-                    <li><a href="#">Category Name</a>
-                    </li>
-                    <li><a href="#">Category Name</a>
-                    </li>
-                    <li><a href="#">Category Name</a>
-                    </li>
-                </ul>
-            </div>
-            <!-- /.col-lg-6 -->
-            <div class="col-lg-6">
-                <ul class="list-unstyled">
-                    <li><a href="#">Category Name</a>
-                    </li>
-                    <li><a href="#">Category Name</a>
-                    </li>
-                    <li><a href="#">Category Name</a>
-                    </li>
-                    <li><a href="#">Category Name</a>
-                    </li>
+                    <?php
+
+                    while($row = mysqli_fetch_assoc($query_result)){
+                        $cat_title = $row['cat_title'];
+                        echo "<li><a href='#'> {$cat_title} </a>
+                        </li>";
+                    }
+
+                    ?>
                 </ul>
             </div>
             <!-- /.col-lg-6 -->
@@ -64,9 +56,6 @@
     </div>
 
     <!-- Side Widget Well -->
-    <div class="well">
-        <h4>Side Widget Well</h4>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore, perspiciatis adipisci accusamus laudantium odit aliquam repellat tempore quos aspernatur vero.</p>
-    </div>
+    <?php include "widget.php"; ?>
 
 </div>
