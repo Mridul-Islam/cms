214088c8f4068772b5df7eefe07e5f246d508469
Mridul-Islam <1610947@iub.edu.bd>
2021-09-03 01:12:06 +0600

    merging

---
+++ includes/login.php
@@ -0,0 +1,59 @@
+<?php include("db.php"); ?>
+<?php session_start(); ?>
+
+<?php
+
+if(isset($_POST['login'])){
+	$username = $_POST['username'];
+	$password = $_POST['password'];
+
+	$escaped_username = mysqli_real_escape_string($connection, $username);
+	$escaped_password = mysqli_real_escape_string($connection, $password);
+
+	$query = "SELECT * FROM users WHERE username='{$escaped_username}' ";
+	$select_user_query = mysqli_query($connection, $query);
+
+	if(!$select_user_query){
+		die("Qeury Failed " . mysqli_error($connection));
+	}
+
+	while($row = mysqli_fetch_assoc($select_user_query)){
+		$db_user_id        = $row['user_id'];
+		$db_username       = $row['username'];
+		$db_user_password  = $row['user_password'];
+		$db_user_firstname = $row['user_firstname'];
+		$db_user_lastname  = $row['user_lastname'];
+		$db_user_role      = $row['user_role'];
+	}
+
+	//$new_password = crypt($password, $db_user_password);
+
+	// if($escaped_username === $db_username && $new_password === $db_user_password){
+	// 	$_SESSION['username'] = $db_username;
+	// 	$_SESSION['firstname'] = $db_user_firstname;
+	// 	$_SESSION['lastname'] = $db_user_lastname;
+	// 	$_SESSION['user_role'] = $db_user_role;
+
+	// 	header("Location: ../admin");
+	// }
+
+
+	if(password_verify($escaped_password, $db_user_password)){
+		$_SESSION['username'] = $db_username;
+		$_SESSION['firstname'] = $db_user_firstname;
+		$_SESSION['lastname'] = $db_user_lastname;
+		$_SESSION['user_role'] = $db_user_role;
+
+		header("Location: ../admin");
+	}
+	else{
+		header("Location: ../index.php");
+	}
+
+
+
+}
+
+
+
+?>
